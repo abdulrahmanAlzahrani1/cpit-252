a39508f168fa14029b11f5faabdde237fc4d5c30
abdulrahman abdullah alzahrani <aalialzahrani0014@stu.kau.edu.sa>
2023-03-26 09:59:56 +0300

    ADD: test for getQuantity test

--- cpit-252-assignment-1/src/test/java/Q1/ProductTest.java
+++ cpit-252-assignment-1/src/test/java/Q1/ProductTest.java
@@ -1,8 +1,25 @@
 package Q1;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
-    
+    @Test
+    public void shouldCountQuantityEqualThree()
+    {
+        Product p1 = new Product(6745, 5.50, "Penne Pasta");
+        Product p2 = new Product(8853, 6.50, "Spaghetti Pasta");
+        Product p3 = new Product(2106, 4.50, "Linguine Pasta");
+        assertEquals(p3.getTotalQuantity(), 3);
+    }
+    public void shouldCountQuantityEqualTow()
+    {
+        Product p1 = new Product(6745, 5.50, "Penne Pasta");
+        Product p2 = new Product(8853, 6.50, "Spaghetti Pasta");
+        assertNotEquals(p2.getTotalQuantity(), 3);
+        assertEquals(p2.getTotalQuantity(),2);
+    }
 
 }
